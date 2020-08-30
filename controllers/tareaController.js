ec216884ea6609243bc426a670560365e8265eb6
Aldimir Aleksandrov <aldimirprincipal@gmail.com>
2020-08-30 13:31:12 +0200

    Actualizar tarea hecho

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -1,6 +1,8 @@
 const Tarea = require('../models/Tarea');
 const Proyecto = require('../models/Proyecto');
-const { validationResult } = require('express-validator')
+const { validationResult } = require('express-validator');
+const { trace } = require('../routes/auth');
+
 // Crea una nueva tarea
 exports.crearTarea = async (req, res) => {
 
@@ -57,9 +59,45 @@ exports.obtenerTareas = async (req, res) => {
         // Obtener las tareas del proyecto
         const tareas = await Tarea.find({ proyecto });
         res.json({ tareas });
-        
+
     } catch (error) {
         console.log(error);
         res.status(500).send('Hubo un error');
     }   
+}
+
+// Actualizar tarea
+exports.actualizarTarea = async (req, res) => {
+    try {
+        // Extraer el proyecto y comprobar si exsite
+        const { proyecto, nombre, estado } = req.body;
+
+        // Si la tarea existe
+        let tarea = await Tarea.findById(req.params.id);
+        if(!tarea) {
+            return res.status(404).json({ msg: 'No existe esa tarea' });
+        }
+
+        // Extraer proyecto
+        const existeProyecto = await Proyecto.findById(proyecto);
+
+        // Revisar si el proyecto actual pretenece al usuario autenticado
+        if(existeProyecto.creador.toString() !== req.usuario.id) {
+            return res.status(401).json({ msg: 'No autorizado' });
+        }
+
+        //Crear nueva tarea
+        const nuevaTarea = {};
+        if(nombre) nuevaTarea.nombre = nombre;
+        if(estado) nuevaTarea.estado = estado;
+
+        // Guardar la tarea
+        tarea = await Tarea.findOneAndUpdate({ _id: req.params.id }, nuevaTarea, { new: true});
+        
+        res.json({ tarea });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error')
+    }
 }
